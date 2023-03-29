29320d021b2171029a39b10cbe6a4fd98ac08cf7
hara <hara@enrapt.jp>
2023-03-29 14:55:34 +0900

    resize

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ function computerVision() {
         // URL images containing printed and/or handwritten text.
         // The URL can point to image files (.jpg/.png/.bmp) or multi-page files (.pdf, .tiff).
         const printedTextSampleURL =
-          "https://github.com/HaraKanon/OCR/blob/main/receipt.JPG";
+          "https://raw.githubusercontent.com/HaraKanon/OCR/main/receipt.png";
 
         // Recognize text in printed image from a URL
         console.log(
